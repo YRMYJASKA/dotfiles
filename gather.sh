95df851262a1b14fe54363905033d3b55e61091d
Jyry Hjelt <hjelt.jyry@gmail.com>
2019-08-25 17:05:42 +0200

    minor adjustment in gather.sh

--- gather.sh
+++ gather.sh
@@ -4,8 +4,8 @@ cp ~/.vimrc ./.vimrc
 cp ~/.zshrc ./.zshrc
 cp ~/.emacs ./.emacs
 
-cp ~/.i3 ./.i3 -r
+cp ~/.i3 . -r
 
 cp ~/.config/i3/config ./.config/i3/config
 
-cp -r ~/Templates/ ./Templates
+cp -r ~/Templates/ .
