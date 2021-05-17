ab86766304979c01ae8c6695cec7cb0e4936c6cb
Jyry Hjelt <hjelt.jyry@gmail.com>
2021-05-17 19:18:49 +0200

    Added new dotfiles from new gentoo installation

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+killall -q polybar
+
+polybar yrmybar
+
+echo "d" > /home/jyry/testing
+
--- gather.sh
+++ gather.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# TODO: Fix this
 
 cp ~/.vimrc ./.vimrc
-cp ~/.zshrc ./.zshrc
+#cp ~/.zshrc ./.zshrc
 cp ~/.doom.d -r  ./.doom.d
 
 cp ~/.config/i3/config ./.config/i3/config
 
 cp -r ~/Templates/ .
 
-cp ~/texmf/tex/latex/local/jyry.sty Templates/jyry.sty
+#cp ~/texmf/tex/latex/local/jyry.sty Templates/jyry.sty
